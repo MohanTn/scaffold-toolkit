45448a5d24ec72983992ed391dd22f24af3cbf54
MohanTn <mohan.tn100@gmail.com>
2026-07-10 14:01:30 +0200

    feat: add built-in Handlebars case conversion helpers

--- packages/core/src/generate/render.ts
+++ packages/core/src/generate/render.ts
@@ -27,7 +27,7 @@ function toPascalCase(str: string): string {
 function toSnakeCase(str: string): string {
   return str
     .replace(/([a-z])([A-Z])/g, '$1_$2')
-    .replace(/[\s\-]+/g, '_')
+    .replace(/[\s-]+/g, '_')
     .toLowerCase();
 }
 
